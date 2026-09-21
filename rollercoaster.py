ec30d05359a9d3eabb01306c287ec7e46ca08901
Nandini Ghule <nandinighule06@gmail.com>
2026-09-21 23:13:35 +0530

    Added rollercoaster height check

---
+++ rollercoaster.py
@@ -0,0 +1,8 @@
+#program to find if a person can ride a rollercoaster based on height
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120 :
+    print("You can ride the RollerCoaster!")
+else:
+    print("Sorry, you need to grow taller to ride the RollerCoaster.")
